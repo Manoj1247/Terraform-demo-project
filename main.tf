d38f5b4be34b2584cebe9d16c06d6bcf9133ae77
Manoj Kalaganti <79302349+Manoj1247@users.noreply.github.com>
2024-02-08 16:10:14 +0000

    changed subnet's cidr block

--- main.tf
+++ main.tf
@@ -46,7 +46,7 @@ resource aws_internet_gateway "main"{
 
 resource "aws_subnet" "main_subnet" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = var.vpc_cidr_block
+  cidr_block              = "${join(".", slice(split(".", var.vpc_cidr_block), 0, 2))}.0.0/24"
   availability_zone       = "us-east-1a"    
 
   tags = {
