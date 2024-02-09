87f64857123a8bd97ad9789b647e708f527ea94c
Manoj1247 <manojsaikalaganti@gmail.com>
2024-02-08 19:29:56 -0800

    added the remaining 2 to moved.tf

--- main.tf
+++ main.tf
@@ -6,11 +6,11 @@ terraform {
     }
   }
 
-    backend "s3" {
-    bucket         = "terraform-demo1-s3-bucket-name"  # Adjust the bucket name
-    key            = "terraform.tfstate"
-    region         = "us-east-1"  # Adjust the region as needed
-    encrypt        = true
+  backend "s3" {
+    bucket  = "terraform-demo1-s3-bucket-name" # Adjust the bucket name
+    key     = "terraform.tfstate"
+    region  = "us-east-1" # Adjust the region as needed
+    encrypt = true
   }
 }
 
@@ -22,8 +22,8 @@ variable "create_internet_gateway_attachment" {
 
 variable "vpc_cidr_block" {
   description = "CIDR block for VPC and Subnet"
-  type = string
-  default = "10.0.0.0/16"
+  type        = string
+  default     = "10.0.0.0/16"
 }
 
 # Configure the AWS Provider
@@ -31,27 +31,9 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_vpc" "main" {
-  cidr_block       = var.vpc_cidr_block
-  instance_tenancy = "default"
 
-  tags = {
-    Name = "main"
-  }
+module "network" {
+  source         = "./modules/network"
+  vpc_cidr_block = var.vpc_cidr_block
 }
-
-resource aws_internet_gateway "main"{
-  vpc_id = aws_vpc.main.id  # Reference the VPC created above
-}
-
-resource "aws_subnet" "main_subnet" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "${join(".", slice(split(".", var.vpc_cidr_block), 0, 2))}.0.0/24"
-  availability_zone       = "us-east-1a"    
-
-  tags = {
-    Name = "main-subnet"
-  }
-}
-
 
