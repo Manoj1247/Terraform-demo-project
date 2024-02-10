bdc7a95893f28809a365812a4cd7fe990a044720
Manoj1247 <manojsaikalaganti@gmail.com>
2024-02-10 13:13:46 -0800

    Use terraform rc with HTTPS username

--- main.tf
+++ main.tf
@@ -33,12 +33,12 @@ provider "aws" {
 
 
 module "network" {
-  source         = "github.com/Manoj1247/terraform-modules-network.git"
+  source         = "git@github.com/Manoj1247/terraform-modules-network.git"
   vpc_cidr_block = var.vpc_cidr_block
 }
 
 module "database" {
-  source      = "github.com/Manoj1247/terraform-modules-database.git"
+  source      = "git@github.com/Manoj1247/terraform-modules-database.git"
   subnet_a_id = module.network.subnet_a_id
   subnet_b_id = module.network.subnet_b_id
 }
