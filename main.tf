bc5b99cedaa820b46a423010eaf9ae464bff6983
Manoj1247 <manojsaikalaganti@gmail.com>
2024-02-10 12:32:34 -0800

    Fix issue with workflow

--- main.tf
+++ main.tf
@@ -37,7 +37,20 @@ module "network" {
   vpc_cidr_block = var.vpc_cidr_block
 }
 
+module "database" {
+  source      = "git@github.com:Manoj1247/terraform-modules-database.git"
+  subnet_a_id = module.network.subnet_a_id
+  subnet_b_id = module.network.subnet_b_id
+}
+
 output "vpc_id" {
   value = module.network.vpc_id
 }
 
+output "subnet_a_id" {
+  value = module.network.subnet_a_id
+}
+
+output "subnet_b_id" {
+  value = module.network.subnet_b_id
+}
