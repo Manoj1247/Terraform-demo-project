4e6a24e5ed328f88cd6b24581dab2b4a5fc90843
Manoj1247 <manojsaikalaganti@gmail.com>
2024-02-12 19:11:08 -0800

    added vpc id

---
+++ locals.tf
@@ -0,0 +1,13 @@
+locals {
+  ecr_repo_name                = "my-first-repo"
+  my_app_cluster_name          = "my-app-cluster"
+  my_app_task_famliy           = "my-app-task"
+  container_port               = 3000
+  my_app_task_name             = "my-app-task"
+  ecs_task_execution_role_name = "my-app-task-execution-role"
+
+  application_load_balancer_name = "cc-my-app-alb"
+  target_group_name              = "cc-my-alb-tg"
+
+  my_app_service_name = "cc-my-app-service"
+}
--- main.tf
+++ main.tf
@@ -9,6 +9,25 @@ module "database" {
   subnet_b_id = module.network.subnet_b_id
 }
 
+module "ecr" {
+  source = "git@github.com:Manoj1247/terraform-modules-ecr.git"
+  ecr_repo_name   = local.ecr_repo_name
+}
+module "ecs" {
+  source                         = "git@github.com:Manoj1247/terraform-modules-ecs.git"
+  my_app_cluster_name            = local.my_app_cluster_name
+  my_app_task_famliy             = local.my_app_task_famliy
+  container_port                 = local.container_port
+  my_app_task_name               = local.my_app_task_name
+  ecs_task_execution_role_name   = local.ecs_task_execution_role_name
+  application_load_balancer_name = local.application_load_balancer_name
+  target_group_name              = local.target_group_name
+  my_app_service_name            = local.my_app_service_name
+  subnet_a_id                    = module.network.subnet_a_id
+  subnet_b_id                    = module.network.subnet_b_id
+  ecr_repo_url                   = module.ecr.ecr_repo_url
+  vpc_id  = module.network.vpc_id
+}
 output "vpc_id" {
   value = module.network.vpc_id
 }
@@ -19,4 +38,8 @@ output "subnet_a_id" {
 
 output "subnet_b_id" {
   value = module.network.subnet_b_id
+}
+
+output "ecr_repo_url" {
+  value = module.ecr.ecr_repo_url
 }
